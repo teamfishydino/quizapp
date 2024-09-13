41a2b7dad842b1174933e84c4b43d1e194804413
devrisby <118779687+devrisby@users.noreply.github.com>
2024-09-13 19:17:38 -0400

    refactor: apply linting/formatting

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,15 @@
-import express from 'express'
+import express from 'express';
 import proxy from 'express-http-proxy';
-import { handler } from './build/handler.js'
+import { handler } from './build/handler.js';
 
-const app = express()
+const app = express();
 
-app.use('/api', proxy(`http://localhost:8000`, {
-    proxyReqPathResolver: (req) => "/api" + req.url
-}));
+app.use(
+	'/api',
+	proxy(`http://localhost:8000`, {
+		proxyReqPathResolver: (req) => '/api' + req.url
+	})
+);
 
 // let SvelteKit handle everything else, including serving prerendered pages and static assets
 app.use(handler);
